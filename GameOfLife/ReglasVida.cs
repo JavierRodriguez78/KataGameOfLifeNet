372fb0f5f9cd5587a0650d24179745f2be73f913
xavi <xavi@geekshubs.com>
2017-12-18 23:58:47 +0100

    Agregar archivos de proyecto.

---
+++ GameOfLife/ReglasVida.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public enum estadoCelula
+    {
+        vivo,
+        muerto
+    }
+
+    public class ReglasVida
+    {
+        public static estadoCelula GetNuevoEstado(estadoCelula estadoActual, int vecinosVivos)
+        {
+
+            /*Refactorizado*/
+            switch(estadoActual)
+            {
+                case estadoCelula.vivo:
+                    if (vecinosVivos < 2 || vecinosVivos > 3)
+                        return estadoCelula.muerto;
+                    break;
+                case estadoCelula.muerto:
+                    if (vecinosVivos == 3)
+                        return estadoCelula.vivo;
+                    break;
+            }
+            return estadoActual;
+
+            /*
+            if (estadoActual == estadoCelula.vivo && vecinosVivos < 2)
+                return estadoCelula.muerto;
+            if (estadoActual == estadoCelula.vivo && vecinosVivos > 3)
+                return estadoCelula.muerto;
+            if (estadoActual == estadoCelula.muerto && vecinosVivos == 3)
+                return estadoCelula.vivo;
+
+            return estadoActual;
+            */
+
+
+           
+        }
+    }
+}
---
+++ ReglasVidaTests/ReglaVidaTest.cs
@@ -0,0 +1,68 @@
+﻿using GameOfLife;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReglasVidaTests
+{
+    [TestFixture]
+    public class ReglaVidaTest
+    {
+        [Test]
+        public void CelulaViva_CuandoHayMenos2Supervivientes_Muere(
+            [Values(0,1)] int vecinosVivos)
+        {
+            var estadoActual = estadoCelula.vivo;
+            
+            estadoCelula nuevoEstado = ReglasVida.GetNuevoEstado(estadoActual, vecinosVivos);
+            Assert.AreEqual(estadoCelula.muerto, nuevoEstado);
+
+        }
+
+        [Test]
+        public void CelulaViva_CuandoHay2o3Supervivientes_Vive([Values(2,3)] int vecinosVivos)
+        {
+            var estadoActual = estadoCelula.vivo;
+            estadoCelula nuevoEstado = ReglasVida.GetNuevoEstado(estadoActual, vecinosVivos);
+            Assert.AreEqual(estadoCelula.vivo, nuevoEstado);
+        }
+        
+        [Test]
+        public void CelulaViva_CuandoMas3Supervivientes_Muere([Range(4,8)] int vecinosVivos)
+        {
+            var estadoActual = estadoCelula.vivo;
+            estadoCelula nuevoEstado = ReglasVida.GetNuevoEstado(estadoActual, vecinosVivos);
+            Assert.AreEqual(estadoCelula.muerto, nuevoEstado);
+        }
+
+        [Test]
+        public void CelulaMuerta_CuandoExactamente3Supervivientes_Vive()
+        {
+            var vecinosVivos = 3;
+            var estadoActual = estadoCelula.muerto;
+            estadoCelula nuevoEstado = ReglasVida.GetNuevoEstado(estadoActual, vecinosVivos);
+            Assert.AreEqual(estadoCelula.vivo, nuevoEstado);
+        }
+
+        [Test]
+        public void CelulaMuerta_Mientrashayamenos3Supervivientes_Muere([Range(0,2)] int vecinosVivos)
+        {
+            var estadoActual = estadoCelula.muerto;
+            estadoCelula nuevoEstado = ReglasVida.GetNuevoEstado(estadoActual, vecinosVivos);
+            Assert.AreEqual(estadoCelula.muerto, nuevoEstado);
+
+        }
+
+        [Test]
+        public void CelulaMuerta_MientrasMas3Supervivientes_Muere([Range(4,8)] int vecinosVivos)
+        {
+            var estadoActual = estadoCelula.muerto;
+            estadoCelula nuevoEstado = ReglasVida.GetNuevoEstado(estadoActual, vecinosVivos);
+            Assert.AreEqual(estadoCelula.muerto, nuevoEstado);
+        }
+
+    }
+}
